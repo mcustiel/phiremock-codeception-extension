bf18a6d35b2ac461fcb074815b31912ec5332b8b
Mariano Custiel <jmcustiel@gmail.com>
2018-01-22 20:36:35 +0100

    Added test

--- tests/tests/acceptance/BasicTestCest.php
+++ tests/tests/acceptance/BasicTestCest.php
@@ -33,4 +33,47 @@ class BasicTestCest
         $I->didNotReceiveRequestsInRemoteService();
         $I->seeRemoteServiceReceived(0, A::getRequest()->andUrl(Is::equalTo('/some/url')));
     }
+
+    public function severalExceptatationsInOneTest(AcceptanceTester $I)
+    {
+        $I->expectARequestToRemoteServiceWithAResponse(
+            Phiremock::on(
+                A::getRequest()->andUrl(Is::equalTo('/potato'))
+            )->then(
+                Respond::withStatusCode(203)->andBody('I am a potato')
+            )
+        );
+        $I->expectARequestToRemoteServiceWithAResponse(
+            Phiremock::on(
+                A::getRequest()->andUrl(Is::equalTo('/tomato'))
+            )->then(
+                Respond::withStatusCode(203)->andBody('I am a tomato')
+            )
+        );
+        $I->expectARequestToRemoteServiceWithAResponse(
+            Phiremock::on(
+                A::getRequest()->andUrl(Is::equalTo('/coconut'))
+            )->then(
+                Respond::withStatusCode(203)->andBody('I am a coconut')
+            )
+        );
+        $I->expectARequestToRemoteServiceWithAResponse(
+            Phiremock::on(
+                A::getRequest()->andUrl(Is::equalTo('/banana'))
+            )->then(
+                Respond::withStatusCode(203)->andBody('I am a banana')
+            )
+        );
+        foreach (['potato', 'tomato', 'banana', 'coconut'] as $item) {
+            $response = file_get_contents('http://localhost:18080/' . $item);
+            $I->assertEquals('I am a ' . $item, $response);
+        }
+        $I->seeRemoteServiceReceived(4, A::getRequest());
+        $I->seeRemoteServiceReceived(1, A::getRequest()->andUrl(Is::equalTo('/potato')));
+        $I->seeRemoteServiceReceived(1, A::getRequest()->andUrl(Is::equalTo('/tomato')));
+        $I->seeRemoteServiceReceived(1, A::getRequest()->andUrl(Is::equalTo('/banana')));
+        $I->seeRemoteServiceReceived(1, A::getRequest()->andUrl(Is::equalTo('/coconut')));
+        $I->didNotReceiveRequestsInRemoteService();
+        $I->seeRemoteServiceReceived(0, A::getRequest());
+    }
 }
