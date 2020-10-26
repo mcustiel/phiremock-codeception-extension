57e4a7fdaa26a721abf3f8ab1a04ff2d161deb3c
Mariano Custiel <jmcustiel@gmail.com>
2020-10-26 20:30:01 +0100

    Cleaned a bit

---
+++ src/PhiremockExtension/CommandBuilder.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Mcustiel\Phiremock\Codeception\Extension;
+
+class CommandBuilder
+{
+    /** @var Config */
+    private $config;
+
+    public function __construct(Config $config)
+    {
+        $this->config = $config;
+    }
+
+    public function build(): array
+    {
+        $path = $this->config->getPhiremockPath();
+        $phiremockPath = is_file($path) ? $path : $path . DIRECTORY_SEPARATOR . 'phiremock';
+
+        $commandLine = [
+            $this->getCommandPrefix() . $phiremockPath,
+            '-i',
+            $this->config->getInterface(),
+            '-p',
+            $this->config->getPort(),
+        ];
+
+        $this->addDebugMode($commandLine);
+        $this->addExpectationsPath($commandLine);
+        $this->addServerFactory($commandLine);
+        $this->addCertificate($commandLine);
+
+        $commandLine[] = '>';
+        $this->addLogFile($commandLine);
+        $commandLine[] = '2>&1';
+
+        return $commandLine;
+    }
+
+    private function getCommandPrefix(): string
+    {
+        return $this->isWindows() ? '' : 'exec ';
+    }
+
+    private function isWindows(): bool
+    {
+        return DIRECTORY_SEPARATOR === '\\';
+    }
+
+    private function addLogFile(array &$commandline): void
+    {
+        $path = $this->config->getLogsPath();
+        $logFile = is_dir($path) ? $path . DIRECTORY_SEPARATOR . self::LOG_FILE_NAME : $path;
+        $commandline[] = $logFile;
+    }
+
+    private function addCertificate(array &$commandline): void
+    {
+        if ($this->config->getCertificatePath()) {
+            $commandline[] = '-t';
+            $commandline[] = $this->config->getCertificatePath();
+            $commandline[] = '-k';
+            $commandline[] = $this->config->getCertificateKeyPath();
+            if ($this->config->getCertificatePassphrase()) {
+                $commandline[] = '-s';
+                $commandline[] = $this->config->getCertificatePassphrase();
+            }
+        }
+    }
+
+    private function addServerFactory(array &$commandline): void
+    {
+        if ($this->config->getServerFactory()) {
+            $commandline[] = '-f';
+            $commandline[] = escapeshellarg($this->config->getServerFactory());
+        }
+    }
+
+    private function addExpectationsPath(array &$commandline): void
+    {
+        $path = $this->config->getExpectationsPath();
+        $expectationsPath = is_dir($path) ? $path : '';
+        if ($expectationsPath) {
+            $commandline[] = '-e';
+            $commandline[] = $expectationsPath;
+        }
+    }
+
+    private function addDebugMode(array &$commandline): void
+    {
+        if ($this->config->isDebugMode()) {
+            $commandline[] = '-d';
+        }
+    }
+}
--- src/PhiremockExtension/PhiremockProcessManager.php
+++ src/PhiremockExtension/PhiremockProcessManager.php
@@ -37,7 +37,8 @@ class PhiremockProcessManager
 
     public function start(Config $config): void
     {
-        $process = $this->initProcess($config);
+        $commandBuilder = new CommandBuilder($config);
+        $process = $this->initProcess($commandBuilder);
         $this->logPhiremockCommand($config->isDebugMode(), $process);
         $process->start();
         $this->processes[$process->getPid()] = $process;
@@ -51,49 +52,9 @@ class PhiremockProcessManager
         }
     }
 
-    private function initProcess(
-        Config $config
-    ): Process {
-        $path = $config->getPhiremockPath();
-        $phiremockPath = is_file($path) ? $path : $path . DIRECTORY_SEPARATOR . 'phiremock';
-        $path = $config->getExpectationsPath();
-        $expectationsPath = is_dir($path) ? $path : '';
-        $path = $config->getLogsPath();
-        $logFile = is_dir($path) ? $path . DIRECTORY_SEPARATOR . self::LOG_FILE_NAME : $path;
-
-        $commandline = [
-            $this->getCommandPrefix() . $phiremockPath,
-            '-i',
-            $config->getInterface(),
-            '-p',
-            $config->getPort(),
-        ];
-        if ($config->isDebugMode()) {
-            $commandline[] = '-d';
-        }
-        if ($expectationsPath) {
-            $commandline[] = '-e';
-            $commandline[] = $expectationsPath;
-        }
-        if ($config->getServerFactory()) {
-            $commandline[] = '-f';
-            $commandline[] = escapeshellarg($config->getServerFactory());
-        }
-
-        if ($config->getCertificatePath()) {
-            $commandline[] = '-t';
-            $commandline[] = $config->getCertificatePath();
-            $commandline[] = '-k';
-            $commandline[] = $config->getCertificateKeyPath();
-            if ($config->getCertificatePassphrase()) {
-                $commandline[] = '-s';
-                $commandline[] = $config->getCertificatePassphrase();
-            }
-        }
-
-        $commandline[] = '>';
-        $commandline[] = $logFile;
-        $commandline[] = '2>&1';
+    private function initProcess(CommandBuilder $builder): Process
+    {
+        $commandline = $builder->build();
 
         if (method_exists(Process::class, 'fromShellCommandline')) {
             return Process::fromShellCommandline(implode(' ', $commandline));
@@ -106,15 +67,5 @@ class PhiremockProcessManager
         if ($debug) {
             echo 'Running ' . $process->getCommandLine() . PHP_EOL;
         }
-    }
-
-    private function getCommandPrefix(): string
-    {
-        return $this->isWindows() ? '' : 'exec ';
-    }
-
-    private function isWindows(): bool
-    {
-        return DIRECTORY_SEPARATOR === '\\';
     }
 }
