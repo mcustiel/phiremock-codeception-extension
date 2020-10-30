ea0d3d41a70c358f9c98b43bad06571d3d86c329
Mariano Custiel <jmcustiel@gmail.com>
2020-10-30 21:41:15 +0100

    Added license note on top of files. Improved gitignore files

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * This file is part of phiremock-codeception-extension.
+ *
+ * phiremock-codeception-extension is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * phiremock-codeception-extension is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with phiremock-codeception-extension.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 namespace Helper;
 
 // here you can define custom actions
